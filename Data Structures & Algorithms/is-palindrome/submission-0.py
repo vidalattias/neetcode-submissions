8557a84230a0fb07803cad37af483f780eacc38e
Vidal Attias <vidal.attias@gmail.com>
2026-08-13 18:59:32 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sl = s.lower()
+        s = []
+        for x in sl:
+            if x in 'qwertyuiopasdfghjklzxcvbnm1234567890':
+                s.append(x)
+
+
+        print(f'New s -> {s}')
+        N = len(s)
+
+        for i in range(N//2):
+            print(f'{i} -> {s[i]};{s[N-i-1]}')
+            if s[i] != s[N-i-1]:
+                return False
+        return True

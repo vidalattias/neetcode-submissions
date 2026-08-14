b75c84daadd5f5dd977c4a26d3c32ba7d5737087
Vidal Attias <vidal.attias@gmail.com>
2026-08-14 16:22:35 +0200

    Add: daily-temperatures - submission-17

---
+++ Data Structures & Algorithms/daily-temperatures/submission-17.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        tt = temperatures
+        N = len(tt)
+
+        res = [0]*N
+
+        for i in range(N-2, -1, -1):
+            j = i+1
+            while j < N and tt[j] <= tt[i]:
+                if res[j] == 0:
+                    j = None
+                    break
+                j += res[j]
+            if j:
+                res[i] = j-i
+        return res
+            
